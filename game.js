6fb162e949811f6b7df412b09392b66e66ec4b80
Rees Payne <rplegos@gmail.com>
2015-07-22 20:47:12 -0400

    save

---
+++ game.js
@@ -0,0 +1,16 @@
+var game = new Phaser.Game(240, 128, Phaser.AUTO, 'scene', { preload: preload, create: create, update: update, render: render });
+
+function preload () { 
+  game.load.image('cave','cave.png');
+
+}
+
+function create () {
+  s = game.add.tileSprite(0, 0, 240, 128, 'cave');
+}
+
+function update () {
+}
+
+function render () {
+}
