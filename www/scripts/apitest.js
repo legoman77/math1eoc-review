29776df8741449c7a17952fa82b8d44425edfc95
Rees Payne <rplegos@gmail.com>
2015-09-04 20:22:38 -0400

    save

--- www/scripts/apitest.js
+++ www/scripts/apitest.js
@@ -1,21 +1,20 @@
 (function(){
 
-//var request = new XMLHttpRequest();
-//request.open('GET', '/questions.json', true);
+var request = new XMLHttpRequest();
+request.open('GET', '/questions.json', true);
 
-//request.onload = function() {
-//  if (request.status >= 200 && request.status < 400) {
-//    var data = JSON.parse(request.responseText);
-//    document.write(data.questions[0].text);
-//  }
-//}
+request.onload = function() {
+  if (request.status >= 200 && request.status < 400) {
+    var data = JSON.parse(request.responseText);
+    var q = document.querySelector(".question");
+    q.innerText = data.questions[0].text;
+  }
+}
 
-var q = document.querySelector(".question");
-q.innerText = "`w^2`"
+request.onerror = function() {
+    console.log("problem officer?");
+}
 
-//request.onerror = function() {
-//    console.log("problem officer?");
-//};
+request.send();
 
-//request.send();
 })();
