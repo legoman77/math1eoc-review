9b9bb653329b5bc7c223bd34f466f3e23c4b8806
Rob Muhlestein <rob@skilstak.com>
2015-09-11 20:49:26 -0400

    save

--- public/testprep.js
+++ public/testprep.js
@@ -4,15 +4,31 @@ var request = new XMLHttpRequest();
 request.open('GET', '/questions.json', true);
 
 var data = null;
+var qfield = document.querySelector("#question")
+
+function randInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 var randomQuestion = function () {
   if (data === null) return null;
+  var i = randInt(0,data.questions.length);
+  return data.questions[i];
+}
+
+var showNextQuestion = function () {
+    var question = randomQuestion();
+    var answers = document.querySelectorAll(".answer");
+    qfield.innerText = question.text;
+    for (n = 0; n < answers.length; n++) {
+      answers[n].innerText = question.answers[n];
+    }
 }
 
 request.onload = function() {
   if (request.status >= 200 && request.status < 400) {
     data = JSON.parse(request.responseText);
-    var q = document.querySelector("#question");
-    q.innerText = data.questions[0].text;
+    return showNextQuestion();
   }
 }
 
