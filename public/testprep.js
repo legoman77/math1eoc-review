348cad5cec8e69649bbc4e1daf766497833e4e1f
Rob Muhlestein <rob@skilstak.com>
2015-09-11 20:15:33 -0400

    save

--- public/testprep.js
+++ public/testprep.js
@@ -1,11 +1,16 @@
 (function(){
 
 var request = new XMLHttpRequest();
-request.open('GET', '/questions.json', false);
+request.open('GET', '/questions.json', true);
+
+var data = null;
+var randomQuestion = function () {
+  if (data === null) return null;
+}
 
 request.onload = function() {
   if (request.status >= 200 && request.status < 400) {
-    var data = JSON.parse(request.responseText);
+    data = JSON.parse(request.responseText);
     var q = document.querySelector("#question");
     q.innerText = data.questions[0].text;
   }
